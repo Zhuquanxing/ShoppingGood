a4127a6a13333f00b2a2b989cf5fffc2c9fe3b42
zhuquanxing <1282361207@qq.com>
2024-06-07 10:46:12 +0800

    jingdong

---
+++ src/views/home/Nearby.vue
@@ -0,0 +1,90 @@
+
+<template>
+  <div class="nearby">
+              <h3 class="nearby title">附近店铺</h3>
+              <div class="nearby_item"
+              v-for="item in nearbyList"
+              :key="item.id">
+
+                 <img :src="item.imgUrl"
+                 class="nearby_item_img" alt="" />
+                 <div class="nearby_content">
+                     <div class="nearby_content_title">{{ item.title }}</div>
+                     <div class="nearby_content_tags">
+                      <span class="nearby_content_tag"
+                      v-for="(innerItem, innerIndex) in item.tags"
+                      :key="innerIndex">{{ innerItem }}</span>
+                     </div>
+                     <p class="nearby_content_highlight">{{ item.desc }}</p>
+                 </div>
+              </div>
+            </div>
+  </template>
+
+<script>
+export default {
+  name: 'Nearby',
+  setup () {
+    const nearbyList = [{
+      id: 1,
+      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
+      title: '沃尼玛',
+      tags: ['月售999+', '起送￥1w', '基础运费￥1w'],
+      desc: 'SSSVIP尊享满100000元减1元运费劵'
+    }, {
+      id: 2,
+      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
+      title: '沃尼玛',
+      tags: ['月售999+', '起送￥10w', '基础运费￥1w'],
+      desc: ' SSSVIP尊享满100000元减1元运费劵 '
+    }]
+    return { nearbyList }
+  }
+}
+</script>
+
+  <style lang="scss" scoped>
+      @import '../../style/viriables.scss';
+   .nearby {
+        .nearby_title {
+          margin: 16px 0 2px 0;
+          font-size: 18px;
+          font-weight: normal;
+          color: $content-fontcolor;
+        }
+        .nearby_item {
+          display: flex;
+          padding-top: 12px;
+        }
+        .nearby_item_img {
+          margin-right: 16px;
+          width: 56px;
+          height: 56px;
+        }
+        .nearby_content {
+          flex: 1;
+          padding-bottom: 12px;
+          border-bottom: 1px solid $content-bgColor;
+        }
+        .nearby_content_title {
+          line-height: 22px;
+          font-size: 16px;
+          color: $content-fontcolor;
+        }
+        .nearby_content_tags {
+          margin-top: 8px;
+          line-height: 18px;
+          font-size: 13px;
+          color: $content-fontcolor;
+        }
+        .nearby_content_tag {
+          margin-right: 16px;
+        }
+        .nearby_content_highlight {
+          margin: 8px 0 0 0;
+          line-height: 18px;
+          font-size: 13px;
+          color: #e93b3b;
+        }
+      }
+  </style>
---
+++ src/views/home/StaticPart.vue
@@ -0,0 +1,123 @@
+<template>
+<div class="position">
+            <span class="iconfont position_icon">&#xe64c;</span>
+             广东邮电职业技术学校
+             <span class="iconfont position_notice">&#xe8fc;</span>
+          </div>
+          <div class="search">
+            <span class="iconfont">&#xe644;</span>
+             <span class="search_text">山姆会员商店优惠商品</span>
+          </div>
+          <div class="banner">
+            <img class="banner_img" src="http://www.dell-lee.com/imgs/vue3/banner.jpg" alt=""/>
+          </div>
+          <div class="icons">
+            <div
+            class="icons_item"
+            v-for="item in iconsList"
+            :key="item.desc">
+              <img class="icons_item_img"
+              :src="'http://www.dell-lee.com/imgs/vue3/${item.imgName}.png'" />
+              <p class="icons_item_desc">{{ item.desc }}</p>
+            </div>
+          </div>
+          <div class="gap"></div>
+</template>
+
+<script>
+export default {
+  name: 'StaticPart',
+  setup () {
+    const iconsList = [
+      { imgName: '超市便利', desc: '超市便利' },
+      { imgName: '菜市场', desc: '菜市场' },
+      { imgName: '签到', desc: '签到' },
+      { imgName: '水果店', desc: '水果店' },
+      { imgName: '鲜花', desc: '鲜花绿植' },
+      { imgName: '医药健康', desc: '医药健康' },
+      { imgName: '家居', desc: '家居时尚' },
+      { imgName: '蛋糕', desc: '烘焙蛋糕' },
+      { imgName: '大牌免运', desc: '大牌免运' },
+      { imgName: '红包', desc: '红包套餐' }
+    ]
+    return { iconsList }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+    @import '../../style/viriables.scss';
+    @import '../../style/mixins.scss';
+.position {
+      position: relative;
+      padding: 16px 24px 16px 0;
+      line-height: 20px;
+      font-size: 16px;
+      @include ellipsis;
+      .position_icon {
+        position: relative;
+        top: 1px;
+        font-size: 20px;
+      }
+      .position_notion {
+         position: absolute;
+         right: 0;
+         top: 17px;
+         font-size: 20px;
+      }
+      color: $content-fontcolor;
+    }
+    .search {
+      margin-bottom: 12px;
+      line-height: 20px;
+      background: #f5f5f5;
+      color: #b7b7b7;
+      border-radius: 16px;
+      .iconfont {
+        position: relative;
+        top: 6px;
+        display: inline-block;
+        padding: 0 12px 0 16px;
+        font-size: 20px;
+      }
+      .search_text {
+        display: inline-block;
+        font-size: 14px;
+      }
+    }
+
+    .banner {
+      height: 0;
+      overflow: hidden;
+      padding-bottom: 25.4%;
+      .banner_img {
+        width: 100%;
+      }
+    }
+
+    .icons {
+      display: flex;
+      flex-wrap: wrap;
+      margin-top: 16px;
+      .icons_item {
+        width: 20%;
+      }
+      .icons_item_img {
+        display: block;
+        width: 40px;
+        height: 40px;
+        margin: 0 auto;
+      }
+      .icons_item_desc {
+        margin: 6px 0 16px 0;
+        text-align: center;
+        color: $content-fontcolor;
+      }
+    }
+    .gap {
+      margin: 0 -18px;
+      height: 2px;
+      background: $content-bgColor;
+    }
+</style>
